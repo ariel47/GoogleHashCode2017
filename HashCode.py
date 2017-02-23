4e71b7b828796202685f7b15da9a9bc03b3f43dc
ariel.s <ariel47@gmail.com>
2017-02-23 23:35:58 +0200

    latest version

--- HashCode.py
+++ HashCode.py
@@ -1,31 +1,35 @@
 import numpy as np
 import os
 import sys, getopt
+import time
 
 CAPACITY = ""
 
 NOT_CONNECTED = np.inf
 
-def algorithm(V, E, R):
+def algorithm(V, E, R, filename):
 
     result = dict()
+
+    E = E[:,-1].reshape(-1,1) - E
+    E[E < 0] = 0
     
     for i in range(len(E[0]) - 1):
         # get the endpoints that are connected to the cache server
-        E_inds = (E[:,i+1] < np.inf) # TODO: make sure this is the right condition
+        E_inds = (E[:,i] < np.inf) # TODO: make sure this is the right condition
         E_inds = np.arange(E.shape[0])[E_inds]
-        e = E[E_inds,:]
-        #print('E_inds', E_inds)
-        #print('e\n',e)
-    
+        endpoints = E[E_inds,:]
+
         # get the requests that come from these endpoints
         R_inds = np.in1d(R[:,1], E_inds)
-        r = R[R_inds,:]
-        if r.size == 0:
+        requests = R[R_inds,:]
+        if requests.size == 0:
             continue
-        #print('R_inds', R_inds)
-        #print('r\n',r)
-    
+
+        # calculate the score - R_N * (L_D -L)
+        latency = endpoints[requests[:,1], i]
+        requests[:,2] = requests[:,2] * latency
+        
         # calculate the number of requests per video
         '''
         # use histogram with number of requests as weights
@@ -33,23 +37,20 @@ def algorithm(V, E, R):
         scores, _ = np.histogram(r[:,0], bins=len(V), weights=r[:,2])
         scores = np.stack
         '''
-    
-        unique_videos = np.unique(r[:,0])
-        #print('unique videos', unique_videos)
-        r_per_video = np.zeros(V.size)
+
+        unique_videos = np.unique(requests[:,0])
+        unique_videos = np.random.choice(unique_videos, min(130, unique_videos.size), replace=False)
+
+        r_per_video = np.zeros(V.size, dtype=np.int64)
         for j in unique_videos:
-            r_j = (r[:,0] == j)
-            #print('r_j\n',r_j)
-            #print('r\n',r[r_j,2])
-            r_per_video[j] = np.sum(r[r_j, 2])
-        #print('r_per_video\n', r_per_video)
-    
+            r_j = (requests[:,0] == j)
+            r_per_video[j] = np.sum(requests[r_j, 2])
+
         # fill the cache server with the most requested videos
+        np.true_divide(r_per_video, V) # divide by size
         r_per_video = np.vstack([np.arange(V.size), r_per_video]).T
-        #print('r_per_video\n', r_per_video)
         order = np.argsort(r_per_video[:,1])[::-1]
         r_per_video = r_per_video[order,:]
-        #print('r_per_video\n',r_per_video)
         result_i = []
         capacity_i = 0
         for video in r_per_video[:,0]:
@@ -58,13 +59,10 @@ def algorithm(V, E, R):
             else:
                 result_i.append(video)
                 capacity_i += V[video]
-    
+
         result[i] = result_i
-        #print('result_i', result_i)
-    
-    #print('result\n', result)
-    
-    with open('result.txt', 'w') as f_out:
+
+    with open(filename + '.txt', 'w') as f_out:
         f_out.write(str(len(result)) + '\n')
         for key, val in result.items():
             f_out.write(str(int(key)) + ' ')
@@ -114,6 +112,7 @@ def parse_input(filename):
 
 
 def main():
+    start = time.time()
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hi:o", ["help=", "ifile=", "ofile"])
     except getopt.GetoptError:
@@ -125,7 +124,10 @@ def main():
             sys.exit()
         if opt in ("ifile", "-i"):
             videos, endpoints, requests = parse_input(arg)
-            algorithm(videos, endpoints, requests)
+            algorithm(videos, endpoints, requests, arg)
+
+    print('time: {:.2f}'.format(time.time() - start))
+
 
 if __name__ == "__main__":
     main()
